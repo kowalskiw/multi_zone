afcead0df1f7f407dd91f76c896de2c4a418f97b
kowalskiw <kowalskiwojciech986@gmail.com>
2020-07-24 13:53:04 +0200

    charts from single OZone results
    .pri and .stt

--- chart.py
+++ chart.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from pandas import read_csv as rcsv
 from os import chdir
+import pandas as pd
+import numpy as np
 
 '''set of charting functions for different purposes'''
 
@@ -73,4 +75,75 @@ class Charting:
         sns_plot = sns.distplot(self.results.time_crit[self.results.time_crit > 0],  axlabel='Czas [s]')
         plt.axvline(x=rset, color='r')
         plt.savefig('dist_d.png')
+
+
+'''DrawOZone allows to create set of charts from OZone data
+use .PRI or .STT output file as an argument'''
+
+class DrawOZone:
+    def __init__(self, file_paths):
+        # if file_paths[-3:] != ('pri' or 'stt'):
+        #     'Use PRI or STT file as an argument, please!'
+        self.file_paths = file_paths
+
+    def read(self, pth):
+        with open(pth) as file:
+            raw = file.readlines()
+
+        data = []
+        for line in raw[2:]:
+            df_line = []  # add time to line
+            for i in range(0, 140, 10):
+                try:
+                    df_line.append(float(line[max(0, i-2):(8+i)].split()[-1]))      # add values to line
+                except IndexError:
+                    df_line.append(0)
+            data.append(df_line)    # add line to table
+
+        df = pd.DataFrame(np.array(data), columns=[raw[0].split()])     # create dataframe
+
+        return df
+
+    def read_stt(self, pth):
+        with open(pth) as file:
+            raw = file.readlines()
+
+        data = []
+        for line in raw[2:]:
+            df_line = []  # add time to line
+            for i in range(0, 30, 10):
+                try:
+                    df_line.append(float(line[max(0, i-2):(8+i)].split()[-1]))      # add values to line
+                except IndexError:
+                    df_line.append(0)
+            data.append(df_line)    # add line to table
+
+        df = pd.DataFrame(np.array(data), columns=[raw[1].split()])     # create dataframe
+
+        return df
+
+    def chart(self, name, dfs):
+        fig, ax = plt.subplots(1, 1)
+        labs = ['50MW', '100MW', '300MW']
+        i = 0
+        for df in dfs:
+            print(list(df))
+            print(list(df).index((name,)))
+            df.plot(x=0, y=list(df).index((name,)), ax=ax, label=labs[i])
+            i += 1
+
+        plt.ylabel = name
+        plt.xlabel = 'time'
+
+        plt.savefig(name)
+
+        return fig
+
+    def draw(self):
+        dfs = []
+        for file in self.file_paths:
+            dfs.append(self.read_stt(file))
+
+        for column in list(dfs[0])[1:]:
+            self.chart(column[0], dfs)
 
